05bd17ddea44a589cb1a99edf50727d18fbe29fb
oopszaxyz <60844631+oopszaxyz@users.noreply.github.com>
2020-02-12 23:58:40 +0700

    Add files via upload

---
+++ pokemon/SelectedPoke.java
@@ -0,0 +1,11 @@
+
+
+class SelectedPoke extends Pokemon{
+    public String PokeName = "";
+	public int Level = 0;
+    public int HP = 0;
+    public int SP = 0;
+    public int Atk = 0;
+    public int Def = 0;
+    public int Skill = 0;
+}
